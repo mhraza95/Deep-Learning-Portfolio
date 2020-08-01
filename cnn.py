b13c69cd97a6714fb361780f9c6b06551d66ebfb
Hassan Raza <mhraza95@gmail.com>
2020-08-01 11:00:25 +0530

    model train 74.346 accuracy

---
+++ cnn.py
@@ -0,0 +1,33 @@
+
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator
+
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(200, 200, 3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+opt = SGD(lr=0.001, momentum=0.9)
+model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+
+# create data generator
+datagen = ImageDataGenerator(rescale=1.0/255.0)
+train_it = datagen.flow_from_directory('dataset/train/',
+                                       class_mode='binary', batch_size=64, target_size=(200, 200))
+test_it = datagen.flow_from_directory('dataset/test/',
+                                      class_mode='binary', batch_size=64, target_size=(200, 200))
+history = model.fit_generator(train_it, steps_per_epoch=len(train_it),
+                              validation_data=test_it, validation_steps=len(test_it), epochs=20, verbose=1)
+_, acc = model.evaluate_generator(test_it, steps=len(test_it), verbose=1)
+print('> %.3f' % (acc * 100.0))
+
+model.save("model.h5")
+print("Saved model to disk")
